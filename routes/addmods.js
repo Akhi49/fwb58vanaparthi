2c2682b2ad54aa389b34103cd581f5203c208def
Akhil Vanaparthi <vanaparthiakhil3@gmail.com>
2021-11-03 18:21:39 -0500

    updated files

--- routes/addmods.js
+++ routes/addmods.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Add Mods' });
+  res.render('addmods', { title: 'Add Mods' });
   let query = req.query 
   console.log(`rows ${query.rows}`) 
   console.log(`cols ${query.cols}`) 
